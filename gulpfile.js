40d6071de2b600f7d87471e1403863c9d256eb57
liupeng <apple@patrickliu.local>
2016-03-12 16:44:20 +0800

    add demo

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gulpCompass = require('gulp-compass');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+var clean = require('gulp-clean');
+var opn = require('opn');
 
 gulp.task('mocha', function() {
     return gulp.src('test/main.js')
@@ -35,4 +39,50 @@ gulp.task('compass', function() {
 });
 
 
-gulp.task('default', ['test']);
+gulp.task('dev', ['cpLib', 'cpAll', 'buildCompass', 'server'], function() {
+
+});
+
+gulp.task('clean', function() {
+
+    return gulp.src('demo/dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('buildCompass', ['clean', 'cpLib', 'cpAll'], function() {
+
+    return gulp.src(__dirname + '/demo/dist/**/*.scss')
+        .pipe( gulpCompass({
+            project: __dirname + '/demo/dist',
+            style: 'expanded',
+            image: 'themes',
+            sass: 'themes',
+            relative: false,
+            css: __dirname + '/demo/dist/themes/css'
+        }))
+            .pipe(gulp.dest('local/themes/default'));
+});
+
+gulp.task('cpLib', ['clean'], function() {
+    return gulp.src('utils.scss')
+    .pipe(rename('_utils.scss'))
+    .pipe(gulp.dest('./demo/src/themes/lib'));
+});
+
+gulp.task('cpAll', ['cpLib', 'clean'], function() {
+
+    return gulp.src('demo/src/**')
+    .pipe(gulp.dest('demo/dist'));
+
+});
+gulp.task('server', function() {
+
+    connect.server({
+        root: 'demo/dist',
+        port: 8080
+    });
+    opn('http://localhost:8080/index.html');
+
+});
+
+gulp.task('default', ['dev']);
